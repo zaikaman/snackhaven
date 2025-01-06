0181c9c99a5f832692e89dacf54ffc6b94c1fffe
zaikaman <91252891+zaikaman@users.noreply.github.com>
2025-01-06 23:42:16 +0700

    Sửa lần 4

--- includes/url_config.php
+++ includes/url_config.php
@@ -6,7 +6,15 @@ $environment = getenv('ENVIRONMENT') ?: 'local';
 $isProduction = $environment === 'host';
 
 // Base URL cho website
-define('BASE_URL', $isProduction ? '' : '/snackhaven');
+if ($isProduction) {
+    // Trên môi trường production (Heroku)
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+    $host = $_SERVER['HTTP_HOST'];
+    define('BASE_URL', '');
+} else {
+    // Trên môi trường local
+    define('BASE_URL', '/snackhaven');
+}
 
 // Hàm helper để tạo URL
 function url($path = '') {
